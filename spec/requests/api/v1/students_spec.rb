6138c9986bdb4199f677d980acc278b3a8d1649d
ricardosilva13 <ricardo.b.silva13@gmail.com>
2017-05-11 12:09:10 -0300

    add teste to #index students

--- spec/requests/api/v1/students_spec.rb
+++ spec/requests/api/v1/students_spec.rb
@@ -4,12 +4,19 @@ RSpec.describe Api::V1::StudentsController, type: :request do
   describe "#index" do
     before(:each) do
       get '/api/v1/students'
-      json = JSON.parse(response.body)
-      students = create_list(:student, 10)
     end
 
-    it "200 status code" do
+    it "return all the students" do
+      students = create_list(:student, 10)
+      expect(students.length).to eq(10)
+    end
+
+    it "returns a successful 200 response" do
       expect(response).to be_success
+    end
+
+    it "return json content type " do
+      expect(response.content_type).to eq("application/json")
     end
   end
 end
